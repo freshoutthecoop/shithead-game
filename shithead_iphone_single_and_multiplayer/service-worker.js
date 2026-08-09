4ac7ddd02a5696d514f033596e93b88c7a4f028d
freshoutthecoop <fr5c8422gh@privaterelay.appleid.com>
2026-08-09 14:04:06 -0400

    Delete shithead_iphone_single_and_multiplayer directory

--- shithead_iphone_single_and_multiplayer/service-worker.js
+++
@@ -1,5 +0,0 @@
-const CACHE='shithead-combined-v1';
-const ASSETS=['./','./index.html','./single.html','./multiplayer.html','./manifest.webmanifest','./icon-180.png','./icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
