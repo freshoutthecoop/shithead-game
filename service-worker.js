5534ee0cb1a5b214d1124060d98a6e676834246a
freshoutthecoop <fr5c8422gh@privaterelay.appleid.com>
2026-08-09 14:34:10 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='shithead-multiplayer-v11';
+const CACHE='shithead-multiplayer-v12-rebuild';
 const ASSETS=['./','./index.html','./single.html','./multiplayer.html','./manifest.webmanifest','./icon-180.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
